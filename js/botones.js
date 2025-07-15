9cde783d1ff68aef617d959a522947ddfb74b251
Luka-dev-sk8 <lukantecao@gmail.com>
2025-07-15 11:32:57 -0300

    origen

---
+++ js/botones.js
@@ -0,0 +1,46 @@
+
+const submenu = document.querySelector('.menu');
+const btn = document.querySelector('#barra_desplegable');
+const aparece = document.querySelector('.aparece');
+
+
+btn.addEventListener('click', () => { 
+    submenu.classList.add('aparece');
+    
+});
+
+document.addEventListener('click', e => {
+  if (submenu.classList.contains('aparece') && !submenu.contains(e.target) && !btn.contains(e.target)){
+        submenu.classList.remove('aparece');
+  }
+});
+
+const cerrar = document.querySelector('#barra_cerrar');
+cerrar.addEventListener('click', () => {
+    submenu.classList.remove('aparece');
+})
+
+  
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const botonF = document.querySelectorAll('.lista_prod');
+  const prod = document.querySelectorAll('.tarjeta');
+
+  botonF.forEach(button => {
+    button.addEventListener('click', () => {
+      const categoria = button.dataset.categoria;
+
+      prod.forEach(prod => {
+        prod.classList.remove('mostrar');
+        if (categoria === 'todo' || prod.dataset.categoria === categoria) {
+          prod.classList.add('mostrar');
+        }
+      });
+    });
+  });
+});
